c743818dcf3b57b28099e7759f37396ce9bc340c
profiejack-ctrl <profiejack@gmail.com>
2026-03-30 11:51:18 +0800

    change db creds

--- connection.js
+++ connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://profiejack_db_user:4czpcqzRGa5K0mph@techtrendz.ylommzi.mongodb.net/shoppers")
+mongoose.connect("mongodb://profiejack_db_user:4czpcqzRGa5K0mph@ac-zkqyl56-shard-00-00.ylommzi.mongodb.net:27017,ac-zkqyl56-shard-00-01.ylommzi.mongodb.net:27017,ac-zkqyl56-shard-00-02.ylommzi.mongodb.net:27017/?ssl=true&replicaSet=atlas-p66pbc-shard-0&authSource=admin&appName=techtrendz")
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
