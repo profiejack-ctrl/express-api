24baa544c3b873155545731eb3150d11f3ad1367
profiejack-ctrl <profiejack@gmail.com>
2026-03-30 11:40:55 +0800

    add db cred

--- connection.js
+++ connection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/shoppers")
+mongoose.connect("mongodb+srv://profiejack_db_user:4czpcqzRGa5K0mph@techtrendz.ylommzi.mongodb.net/shoppers")
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
